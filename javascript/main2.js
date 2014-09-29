055b03d0c88a476c2864b1207b3c7a290fe64492
Stephan Yu <Stephan.yu@gmail.com>
2014-09-29 00:15:00 -0700

    added test users and started writing functions filterOOgles, and addPreferences

---
+++ javascript/main2.js
@@ -0,0 +1,171 @@
+var isNewUser = true;
+var currentUser = null;
+var currentUserId = null;
+var refRoot = new Firebase("https://tinder.firebaseio.com");
+var refUsers = new Firebase("https://tinder.firebaseio.com/users/");
+var refLocation = new Firebase("https://tinder.firebaseio.com/geoLocation");
+var refChat = new Firebase("https://tinder.firebaseio.com/chat");
+
+
+$(document).ready(function() {
+  seedUsers();
+});
+
+var authClient = new FirebaseSimpleLogin(refRoot, function(error, user) { 
+  // log error if there is an error
+  if (error) {
+    console.log("Firebase simple login error:" + error);
+  // if user exists, begin user authenticateion with Firebase
+  } else if (user) {
+    console.log(user);
+    var objUser = new Firebase( refUsers + user.uid );
+    
+    objUser.once('value', function(snapshot) {
+      currentUser = snapshot.val();
+      console.log("Snapshot of objUser:" + currentUser);
+    });
+
+      // // if currentUser exists then change isNewUser = false
+    if ( currentUser !== null && currentUser !== '' && currentUser !== undefined) {
+      console.log("false event");
+      isNewUser = false;
+    }
+
+    // check refUsers if user exists
+    if ( isNewUser ) {
+      // save new user's profile into Firebase under their uid
+      refUsers.child(user.uid).set({
+        provider: user.provider,
+        providerId: user.id,
+        displayName: user.displayName,
+        // firstName: user.first_name,
+        gender: user.thirdPartyUserData.gender,
+        age: user.thirdPartyUserData.age_range.min,
+        picUrl: user.thirdPartyUserData.picture.data.url
+      });
+
+    }
+      currentUser = user;
+      currentUserId = user.uid;
+      console.log("User ID: " + currentUserId + ", Provider: " + user.provider);
+
+      // $("#main-go-oogle").attr("style", "");
+      // $("#go-oogle-login").attr("style", "display: none");
+  } else {
+    // user is logged out
+    console.log("user logged out");
+  }
+      addPreferences();
+});
+
+$("#fb-login").on('click', function() {
+  authClient.login('facebook', {
+    preferRedirect: true,
+    rememberMe: true,
+    scope: 'email,user_likes,user_friends'
+  });
+
+});
+
+
+// $("#demo-btn").on('click', addPreferences);
+
+
+var genderPref;
+$('.btn-group .btn').click(function() {
+ genderPref = $(this).attr("value");
+ console.log(genderPref);
+});
+
+
+function addPreferences() {
+  var minAgePref = $('#min-age-pref').val();
+  console.log(minAgePref);
+  var maxAgePref = $('#max-age-pref').val();
+  console.log(maxAgePref);
+
+  // var refPreferences = new Firebase(refUsers + currentUserId);
+
+  refUsers.child(currentUserId).child("preferences").update(
+    { 
+      gender: genderPref, 
+      min_age: minAgePref, 
+      max_age: maxAgePref
+    }
+    );
+
+}
+
+function seedUsers() {
+  
+  refUsers.set({
+  amberuid: {
+    firstName: "Amber",
+    gender: "female",
+    age: 20,
+    photos: {
+      1: "http://static.hdw.eweb4.com/media/wp_400/1/1/3245.jpg",
+      2: "img/amber1.jpg",
+      3: "img/amber2.jpg",
+      4: "img/amber3.jpg"
+    }},
+  blakeuid: {
+    firstName: "Blake",
+    gender: "female",
+    age: 28,
+    photos: {
+      1: "img/blake1.jpg",
+      2: "img/blake2.jpg",
+      3: "img/blake3.jpg",
+      4: "img/blake4.jpg"
+    }},
+  zoeuid: {
+    firstName: "Zoe",
+    gender: "female",
+    age: 25,
+    photos: {
+      1: "img/zoe1.jpg",
+      2: "img/zoe2.jpg",
+      3: "img/zoe3.jpg",
+      4: "img/zoe4.jpg"
+    }}
+  });
+}
+
+var usersArray = [];
+
+// function filterOogles() {
+// console.log("filterOogles");
+//   refUsers.on('value', 
+//     function (snapshot) {
+//       var userObject = snapshot.val();
+//     },
+//     function (errorObject) {
+//       console.log('The read failed: ' + errorObject.code);
+//     }
+//   );
+
+//   $.each(userObject, function (userId, singleUserObject) {
+
+//     if ( singleUserObject.gender === currentUserId.preferences.gender && singleUserObject.age > currentUserId.preferences.min_age && singleUserObject.age < currentUserId.preferences.max_age) {
+
+//       // console.log(singleUserObject.firstName);
+//       // store user in "meets_criteria" table of specified user
+//       // stephanRef = new Firebase("https://tinder.firebaseio.com/users/" + currentUser + "meets_criteria/" + userId);
+
+//       refUsers.child(currentUserId).child("meets_criteria").child(userId).set( {
+//          age: singleUserObject.age,
+//          firstName: singleUserObject.firstName,
+//          // picUrl: singleUserObject.picUrl
+//       });
+
+//     } else {
+//       console.log("user does not meet criteria");
+//     }
+
+//   });
+
+// }
+
+
+
